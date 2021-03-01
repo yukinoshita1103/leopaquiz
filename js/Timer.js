c7ca2ef7cd5ce93771a3e34d6c8fe3bb11d0e824
ktnghm <63536538+yukinoshita1103@users.noreply.github.com>
2021-03-02 06:04:42 +0900

    Add files via upload

---
+++ js/Timer.js
@@ -0,0 +1,18 @@
+let originalTime = 999;
+// アロー関数で記載。
+let countDown = () => {
+  originalTime--;
+};
+// 初期値を表示。
+let timerElement = document.getElementById("timer-text");
+timerElement.innerText = originalTime;
+
+// 1秒ごとに、1減らして、表示する。タイムアウトで画面遷移。
+let id = setInterval(function () {
+  countDown();
+  timerElement.innerText = originalTime;
+  if (originalTime < 1) {
+    clearInterval(id);
+    window.location.href = "./consequence.html";
+  }
+}, 1000);
